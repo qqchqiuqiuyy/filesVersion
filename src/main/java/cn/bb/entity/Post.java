af5fc90e811d508ea0547565f6aae02c07876d35
bobo_glb <ily520wan_glb>
2020-03-06 22:46:04 +0800

    add elas

--- src/main/java/cn/bb/entity/Post.java
+++ src/main/java/cn/bb/entity/Post.java
@@ -3,20 +3,38 @@ package cn.bb.entity;
 import lombok.Data;
 import lombok.ToString;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+
 import java.sql.Timestamp;
 
 @Data
 @ToString
+@Document(indexName = "post",type = "docs",shards = 1,replicas = 0)
 public class Post {
+    @Id
     private Integer pid;
+
     private Integer uid;
+    @Field(type = FieldType.String,analyzer = "ik_max_word")
     private String postTitle;
+    @Field(type = FieldType.Auto)
     private String postUserName;
+    @Field(type = FieldType.Auto)
     private Timestamp postCreatTime;
+    @Field(type = FieldType.Auto)
     private String path;
+    @Field(type = FieldType.Auto)
     private String postFileName;
+    @Field(type = FieldType.Auto)
     private String postContent;
+    @Field(type = FieldType.Auto)
     private Integer commentNums;
+    @Field(type = FieldType.Auto)
     private String collegeName;
+    @Field(type = FieldType.Auto)
     private Integer collegeId;
 }
